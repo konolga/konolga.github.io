dd8747eb000fad22d2a291315d3f46f191f92fce
Olga Kon <konolga@users.noreply.github.com>
2019-02-16 11:57:12 +0200

    added portfolio

--- js/custom.js
+++ js/custom.js
@@ -34,3 +34,29 @@ $(function () {
         autoplayHoverPause: true
     });
 });
+
+
+/*==================================
+            PORTFOLIO MODAL
+==================================*/
+    //bootstrap portfolio modal
+    $('#portfolio-modal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var workName = button.data('name'); // Extract info from data-* attributes
+        $(this).find('.modal-body').hide();
+        $('.modal-body[data-name = ' + workName + ']').show();
+    });
+
+  //open bootstrap modal from modal
+  $(document).on('hidden.bs.modal', '.modal', function () {
+    if($('.modal:visible').length){
+        $(document.body).addClass('modal-open');
+         if($(window).width() > 991){
+             $(document.body).css({paddingRight: '17px'});
+         }
+    }else {
+        $(document.body).css({paddingRight: 0});
+    }
+});
+
+
